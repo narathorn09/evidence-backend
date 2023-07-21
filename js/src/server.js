6cd85e70d22ae948e77ad9a535f746f3c401cc64
Narathorn Noophum <6310210181@psu.ac.th>
2023-07-21 21:32:22 +0700

    finish upload photo

--- js/src/server.js
+++ js/src/server.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -8,16 +17,61 @@ const http_1 = require("http");
 const corsOptions_1 = require("../config/corsOptions");
 const credentials_1 = require("./middleware/credentials");
 const routerv1_1 = __importDefault(require("./router/routerv1"));
+const path_1 = __importDefault(require("path"));
+const fs_1 = __importDefault(require("fs"));
+const uuid_1 = require("uuid");
 const cookieParser = require("cookie-parser");
 const chalk = require("chalk");
 const cors = require("cors");
-const { server: { port }, } = require("config");
+const { server: { port, host }, } = require("config");
 const app = (0, express_1.default)();
 app.use(credentials_1.credentials);
 app.use(cors(corsOptions_1.corsOptions));
 // app.use(express.urlencoded({extended: true }));
-app.use(express_1.default.json());
+app.use(express_1.default.json({ limit: "100mb" }));
 app.use(cookieParser());
+app.use("/asset", express_1.default.static(path_1.default.join(__dirname, "../uploads")));
+app.post("/api/v1/uploads", (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { evidence_list } = req.body;
+        console.log("evidence_list", evidence_list);
+        const uploadedImages = [];
+        evidence_list.forEach((evidence, index) => {
+            const uploadedEvidence = {
+                evidence_factor: [],
+                evidence_amount: evidence.evidence_amount,
+                type_e_id: evidence.type_e_id,
+                type_e_name: evidence.type_e_name, // Access the type_e_name property from evidence_list
+            };
+            evidence.evidence_factor.forEach((ef, i) => {
+                const filename = `${(0, uuid_1.v1)()}.png`;
+                const pathImg = `${__dirname}/../uploads/${filename}`;
+                if (ef.ef_photo) {
+                    const base64Data = ef.ef_photo.replace(/^data:([A-Za-z-+/]+);base64,/, "");
+                    fs_1.default.writeFileSync(pathImg, base64Data, { encoding: "base64" });
+                    uploadedEvidence.evidence_factor.push({
+                        // url: `${host}/asset/${filename}`,
+                        ef_photo: `${filename}`,
+                        ef_detail: ef.ef_detail,
+                    });
+                }
+                else if (ef.ef_photo === null) {
+                    uploadedEvidence.evidence_factor.push({
+                        ef_photo: null,
+                        ef_detail: ef.ef_detail,
+                    });
+                }
+            });
+            uploadedImages.push(uploadedEvidence);
+        });
+        res.status(200).json({
+            result: uploadedImages || [],
+        });
+    }
+    catch (err) {
+        next(err);
+    }
+}));
 app.get("/", (req, res, next) => {
     res.send("Hello, API For Express & MySQL");
 });
--- src/server.ts
+++ src/server.ts
@@ -3,19 +3,93 @@ import { createServer, Server } from "http";
 import { corsOptions } from "../config/corsOptions";
 import { credentials } from "./middleware/credentials";
 import routerv1 from "./router/routerv1";
+import path from "path";
+import fs from "fs";
+import { v1 as uuid } from "uuid";
 const cookieParser = require("cookie-parser");
 const chalk = require("chalk");
 const cors = require("cors");
 const {
-  server: { port },
+  server: { port, host },
 } = require("config");
 
 const app: Application = express();
 app.use(credentials);
 app.use(cors(corsOptions));
 // app.use(express.urlencoded({extended: true }));
-app.use(express.json());
+app.use(express.json({ limit: "100mb" }));
 app.use(cookieParser());
+app.use("/asset", express.static(path.join(__dirname, "../uploads")));
+
+interface Evidence {
+  ef_photo: string | null;
+  ef_detail: string;
+}
+
+interface UploadedEvidence {
+  evidence_factor: { ef_photo: string | null; ef_detail: string }[];
+  evidence_amount: number; // Assuming evidence_amount is a number, change the type accordingly if needed
+  type_e_id: string; // Assuming type_e_id is a string, change the type accordingly if needed
+  type_e_name: string; // Assuming type_e_name is a string, change the type accordingly if needed
+}
+
+interface EvidenceList {
+  evidence_factor: Evidence[];
+  evidence_amount: number;
+  type_e_id: string;
+  type_e_name: string;
+}
+
+app.post(
+  "/api/v1/uploads",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { evidence_list } = req.body;
+      console.log("evidence_list", evidence_list);
+      const uploadedImages: {}[] = [];
+
+      evidence_list.forEach((evidence: EvidenceList, index: number) => {
+        const uploadedEvidence: UploadedEvidence = {
+          evidence_factor: [], // Corrected declaration
+          evidence_amount: evidence.evidence_amount, // Access the evidence_amount property from evidence_list
+          type_e_id: evidence.type_e_id, // Access the type_e_id property from evidence_list
+          type_e_name: evidence.type_e_name, // Access the type_e_name property from evidence_list
+        };
+
+        evidence.evidence_factor.forEach((ef: any, i: number) => {
+          const filename = `${uuid()}.png`;
+          const pathImg = `${__dirname}/../uploads/${filename}`;
+
+          if (ef.ef_photo) {
+            const base64Data = ef.ef_photo.replace(
+              /^data:([A-Za-z-+/]+);base64,/,
+              ""
+            );
+            fs.writeFileSync(pathImg, base64Data, { encoding: "base64" });
+
+            uploadedEvidence.evidence_factor.push({
+              // url: `${host}/asset/${filename}`,
+              ef_photo: `${filename}`,
+              ef_detail: ef.ef_detail,
+            });
+          } else if (ef.ef_photo === null) {
+            uploadedEvidence.evidence_factor.push({
+              ef_photo: null,
+              ef_detail: ef.ef_detail,
+            });
+          }
+        });
+        uploadedImages.push(uploadedEvidence);
+      });
+
+      res.status(200).json({
+        result: uploadedImages || [],
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+);
 
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.send("Hello, API For Express & MySQL");
